d276f0ed9a04412e2214c0b5ff752426b939e6df
mchacong <mchacong@akamai.com>
2022-02-03 11:14:14 -0600

    add event handlers

--- multiplayer.js
+++ multiplayer.js
@@ -1,5 +1,6 @@
 var player = {
     instances: [],
+    eventsOfInterest: ['onerror', 'onplaying'],
     add: function (container, src) {
         var mediaElement = this.createElement(container)
         mediaElement.src = src
@@ -7,6 +8,7 @@ var player = {
         var hls = new Hls()
         hls.loadSource(src)
         hls.attachMedia(mediaElement)
+        this.attachListeners(mediaElement)
         this.instances.push(hls)
     },
 
@@ -19,5 +21,28 @@ var player = {
         parent.appendChild(video)
 
         return video
+    },
+
+    attachListeners: function (mediaElement) {
+        this.eventsOfInterest.forEach(function (event) {
+            mediaElement[event] = this.eventHandler.bind(this)
+        }.bind(this))
+    },
+
+    eventHandler: function (event) {
+        var target = event.target
+        var msg = '[' + event.type + '] - ' + target.id
+        
+        switch (event.type) {
+            case 'error':
+                this.logger(msg, event.target.error)
+                break;
+            default: this.logger(msg, event)
+                break;
+        }
+    },
+
+    logger: function (msg, metadata) {
+        console.log(msg, metadata)
     }
 };
